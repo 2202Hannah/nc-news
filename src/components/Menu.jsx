fd9d19ee0a13ff779b71d67c9f712971de908470
Hannah Barber <hannahbarber22@hotmail.co.uk>
2023-12-07 20:32:52 +0000

    dropdown icon

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -19,7 +19,12 @@ const Menu = () => {
 
   return (
     <div className="dropdown">
-      <button onClick={handleOpen}>Menu</button>
+        <img
+          className="menu_img"
+          src={require("./images/menu.jpg")}
+          alt="hamburger menu icon"
+          onClick={handleOpen}
+        />
       {open ? (
         <ul className="menu">
           {topics.map((topic, index) => {
